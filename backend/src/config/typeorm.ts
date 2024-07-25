3748998371a02877c5ee2edc43f3983c212fff1e
Emanuele-Zanni <emazanni2022@gmail.com>
2024-07-25 03:19:07 -0300

    CRUD + Entity + Repository created

--- backend/src/config/typeorm.ts
+++ backend/src/config/typeorm.ts
@@ -9,18 +9,18 @@ import { config as dotenvConfig } from 'dotenv';
 dotenvConfig({ path: '.env.development' });
 
 const config = {
-    type: 'postgres',
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    entities: [User, Product, Order, OrderDetail],
-    migrations: ['dist/migrations/*{.ts, .js}'],
-    autoLoadEntities: true,
-    logging: ["error"],
-    synchronize: true,
-    dropSchema: true
-}
+  type: 'postgres',
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  entities: [User, Product, Order, OrderDetail],
+  migrations: ['dist/migrations/*{.ts, .js}'],
+  autoLoadEntities: true,
+  logging: ['error'],
+  synchronize: true,
+  dropSchema: true,
+};
 export const typeOrmConfig = registerAs('typeorm', () => config);
 export const conectionSource = new DataSource(config as DataSourceOptions);
--- backend/src/modules/categories/categories.controller.ts
+++ backend/src/modules/categories/categories.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -7,28 +16,31 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @Get()
+  findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
+    if (page && limit) this.categoriesService.findAll(page, limit);
+    return this.categoriesService.findAll(page, limit);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') category_id: string) {
+    return this.categoriesService.findOne(category_id);
+  }
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
   }
 
-  @Get()
-  findAll() {
-    return this.categoriesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoriesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoriesService.update(+id, updateCategoryDto);
+  @Patch(':id') //? Posiblemente este endpoint sea redundante/inutil
+  update(
+    @Param('id') category_id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return this.categoriesService.update(category_id, updateCategoryDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoriesService.remove(+id);
+  remove(@Param('id') category_id: string) {
+    return this.categoriesService.remove(category_id);
   }
 }
