715757c17cd7f86d45ee229ef3c76ffb2592cd61
Eric David Diaz <91031571+Euka-e@users.noreply.github.com>
2024-07-26 18:24:49 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,10 +7,9 @@ import { UsersModule } from './modules/users/users.module'
 import { AuthModule } from './modules/auth/auth.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { CategoriesModule } from './modules/categories/categories.module';
-import { FileUploadModule } from './modules/file-upload/file-upload.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, AuthModule, OrdersModule, CategoriesModule, FileUploadModule, ConfigModule.forRoot()],
+  imports: [UsersModule, ProductsModule, AuthModule, OrdersModule, CategoriesModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
