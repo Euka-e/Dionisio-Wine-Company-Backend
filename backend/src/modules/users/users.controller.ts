6e604f877a424bde1c2e99538a529b9ded8e6635
Emanuele-Zanni <emazanni2022@gmail.com>
2024-07-27 19:48:20 -0300

    Users CRUD completed

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -9,28 +18,29 @@ import { ApiTags } from '@nestjs/swagger';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Get()
+  findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
+    return this.usersService.findAll(page, limit);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') user_id: string) {
+    return this.usersService.findOne(user_id);
+  }
+
+  //? Crear @Get para findByMail ?
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(@Param('id') user_id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(user_id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  remove(@Param('id') user_id: string) {
+    return this.usersService.remove(user_id);
   }
 }
--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly UsersRepository: UsersRepository) {}
+  findAll(page: number, limit: number) {
+    return this.UsersRepository.getUsers(page, limit);
+  }
+
+  findOne(user_id: string) {
+    return this.UsersRepository.getUserById(user_id);
+  }
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.UsersRepository.createUser(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  update(user_id: string, updateUserDto: UpdateUserDto) {
+    return this.UsersRepository.updateUser(user_id, updateUserDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(user_id: string) {
+    return this.UsersRepository.deleteUser(user_id);
   }
 }
