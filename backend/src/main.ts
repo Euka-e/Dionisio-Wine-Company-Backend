db7e0a95cf7b450d2e26088e791f32ead00c9bdf
Eric David Diaz <ericdiazeuka@gmail.com>
2024-08-09 13:08:40 -0300

    Cors configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,17 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as session from 'express-session';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración de CORS
+  app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
+  }));
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Dionisio Wine Company')
@@ -15,13 +23,14 @@ async function bootstrap() {
     .build()
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
   }));
-  
+
   app.use(
     session({
       secret: process.env.JWT_SECRET,
@@ -29,7 +38,8 @@ async function bootstrap() {
       saveUninitialized: false,
     }),
   );
+
   await app.listen(3000);
-  console.log('Aplication running on port 3000')
+  console.log('Application running on port 3000');
 }
 bootstrap();
