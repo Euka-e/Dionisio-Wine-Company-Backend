73794c7d09a60d793166b7bbe38c629de77f4b20
Eric David Diaz <ericdiazeuka@gmail.com>
2024-08-09 22:10:22 -0300

    Update orders.controller.ts

--- backend/src/modules/orders/orders.controller.ts
+++ backend/src/modules/orders/orders.controller.ts
@@ -8,43 +8,32 @@ import {
   UseGuards,
   Get,
   NotFoundException,
+  BadRequestException,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
-import { ApiTags } from '@nestjs/swagger';
-import { Roles } from 'src/decorators/roles.decorator';
-import { Role } from '../users/dto/roles.enum';
-import { AuthGuard } from '../auth/guards/authorization.guard';
-import { RolesGuard } from '../auth/guards/role.guard';
 import { CreateOrderDto } from './dto/create-order.dto';
 
 @Controller('orders')
-@ApiTags('Orders')
 export class OrdersController {
   constructor(
     private readonly ordersService: OrdersService,
     private readonly usersRepository: UsersRepository
   ) { }
 
-  @Get('/orders')
-  @Roles(Role.User)
-  @UseGuards(AuthGuard, RolesGuard)
-  async getOrders() {
-    return await this.ordersService.getOrders();
-  }
-
   @Post('create/:userId')
-  @Roles(Role.User)
-  @UseGuards(AuthGuard, RolesGuard)
   async create(
     @Param('userId') userId: string,
-    @Body() cartItems: CreateOrderDto
+    @Body() createOrderDto: CreateOrderDto
   ) {
-    const findUser = await this.usersRepository.getUserById(userId);
-    if (findUser) {
-      const order = await this.ordersService.createOrderFromCart(cartItems, userId);
-      return order;
-    } else {
-      throw new NotFoundException('Order creation failed');
+    const user = await this.usersRepository.getUserById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    try {
+      return await this.ordersService.createOrderFromCart(createOrderDto, userId);
+    } catch (error) {
+      throw new BadRequestException('Error creating order');
     }
   }
 }
