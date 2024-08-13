697395004624b5a91ff1524b1fe879995e3c4d41
Emanuele-Zanni <emazanni2022@gmail.com>
2024-08-13 10:17:18 -0300

    Added email confirmation after purchase

--- backend/src/modules/orders/orders.repository.ts
+++ backend/src/modules/orders/orders.repository.ts
@@ -8,6 +8,7 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { Product } from '../products/entities/product.entity';
 import { Repository, DataSource } from 'typeorm';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { MailingService } from '../mailing/mailing.service';
 
 @Injectable()
 export class OrdersRepository {
@@ -20,20 +21,22 @@ export class OrdersRepository {
     @InjectRepository(OrderDetail)
     private readonly orderDetailRepository: Repository<OrderDetail>,
     private readonly usersRepository: UsersRepository,
-  ) { }
-
+    private readonly mailingService: MailingService,
+  ) {}
 
   async getOrders() {
-    const orders = await this.orderRepository.find({ relations: ['user', 'details'] });
+    const orders = await this.orderRepository.find({
+      relations: ['user', 'details'],
+    });
     return orders;
   }
 
   async getOrdersByUserId(userId: string) {
     const orders = await this.orderRepository.find({
       where: { user: { id: userId } },
-      relations: ['user', 'details', 'details.product']
+      relations: ['user', 'details', 'details.product'],
     });
-    const sanitizedOrders = orders.map(order => {
+    const sanitizedOrders = orders.map((order) => {
       const sanitizedUser = { ...order.user }; //? Aquí vamos a poner los campos del usuario que no queremos obtener
       delete sanitizedUser.password;
       delete sanitizedUser.role;
@@ -60,32 +63,47 @@ export class OrdersRepository {
       order.status = OrderStatus.PENDING;
       order.price = cartItems.price;
       const savedOrder = await this.orderRepository.save(order);
-      const orderDetails = await Promise.all(cartItems.cartItems.map(async (item) => {
-        const orderDetail = new OrderDetail();
-        orderDetail.order = savedOrder;
-        const findProduct = await this.productRepository.findOneBy({ productId: item.productId });
-        orderDetail.product = findProduct;
-        orderDetail.quantity = item.quantity;
-        orderDetail.price = item.price;
-        orderDetail.total = item.quantity * item.price;
-        return orderDetail;
-      }));
+      const orderDetails = await Promise.all(
+        cartItems.cartItems.map(async (item) => {
+          const orderDetail = new OrderDetail();
+          orderDetail.order = savedOrder;
+          const findProduct = await this.productRepository.findOneBy({
+            productId: item.productId,
+          });
+          orderDetail.product = findProduct;
+          orderDetail.quantity = item.quantity;
+          orderDetail.price = item.price;
+          orderDetail.total = item.quantity * item.price;
+
+          try {
+            await this.mailingService.sendPurchaseConfirmationEmail(
+              findUser.email,
+            );
+            console.log('Correo de bienvenida enviado correctamente');
+          } catch (mailError) {
+            console.error('Error al enviar el correo:', mailError.message);
+          }
+
+          return orderDetail;
+        }),
+      );
       await this.orderDetailRepository.save(orderDetails);
       /* await this.cartRepository.clearCart(userId); */
       return savedOrder;
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
       throw new BadRequestException('Error creating order');
     }
   }
 
   async updateOrderStatus(order: UpdateOrderDto) {
-    return await this.orderRepository.update({ id: order.orderId }, { status: order.status });
+    return await this.orderRepository.update(
+      { id: order.orderId },
+      { status: order.status },
+    );
   }
 
   async deleteOrdersFromUser(userId: string) {
     return await this.orderRepository.delete({ user: { id: userId } });
   }
-
 }
-
