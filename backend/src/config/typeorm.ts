c0fc24c3e093a9900b7ff818456780b473ae5bb8
Eric David Diaz <91031571+Euka-e@users.noreply.github.com>
2024-09-09 15:50:09 -0300

    Update typeorm.ts

--- backend/src/config/typeorm.ts
+++ backend/src/config/typeorm.ts
@@ -8,6 +8,8 @@ import { Category } from 'src/modules/categories/entities/category.entity';
 import { config as dotenvConfig } from 'dotenv';
 import { Cart } from 'src/modules/cart/entities/cart.entity';
 import { CartDetail } from 'src/modules/cart/entities/cartdetail.entity';
+import { CartItem } from 'src/modules/cart/entities/cart.item.entity';
+import { DiscountCode } from 'src/modules/offers/entities/discountCode.entity';
 
 dotenvConfig({ path: '.env.development' });
 
@@ -20,13 +22,13 @@ const config = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     ssl: process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : false,
-    entities: [User, Product, Order, OrderDetail, Category, Cart, CartDetail],
+    entities: [User, Product, Order, OrderDetail, Category, Cart, CartDetail,CartItem, DiscountCode],
     migrations: ['dist/migrations/*{.ts,.js}'],
     autoLoadEntities: true,
     logging: ['error'],
     synchronize: true,
     dropSchema: true,
-    dropDatabase: false
+    dropDatabase: true
 };
 export const typeOrmConfig = registerAs('typeorm', () => config);
 export const conectionSource = new DataSource(config as DataSourceOptions);
